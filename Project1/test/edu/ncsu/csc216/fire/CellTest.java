b11794fd72cb24924e48ade496db2f495bc407cc
Andrew Kofink <ajkofink@gmail.com>
2011-09-19 16:56:59 -0400

    Starting iteration 1

---
+++ Project1/test/edu/ncsu/csc216/fire/CellTest.java
@@ -0,0 +1,27 @@
+package edu.ncsu.csc216.fire;
+
+import junit.framework.TestCase;
+
+public class CellTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void testGetState() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetState() {
+		fail("Not yet implemented");
+	}
+
+	public void testCopy() {
+		fail("Not yet implemented");
+	}
+
+	public void testSpread() {
+		fail("Not yet implemented");
+	}
+
+}
