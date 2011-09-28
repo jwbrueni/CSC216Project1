00117294f32602d1bad6892594969ab608a7da41
unknown <James@.(none)>
2011-09-28 18:42:58 -0400

    Test commit

---
+++ Project1/test/edu/ncsu/csc216/fire/DriverTest.java
@@ -0,0 +1,19 @@
+package edu.ncsu.csc216.fire;
+
+import junit.framework.TestCase;
+
+public class DriverTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void testSimulate() {
+		fail("Not yet implemented");
+	}
+
+	public void testReplicate() {
+		fail("Not yet implemented");
+	}
+
+}
